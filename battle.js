d7df79e0c73900769d02b40fc5d1702c2ca0e2bf
CoolCatt2000 <liamcouch74@icloud.com>
2025-05-26 22:08:01 +0100

    .

--- battle.js
+++ battle.js
@@ -1,6 +1,10 @@
-// Get the battle code from the URL
 const urlParams = new URLSearchParams(window.location.search);
 const battleCode = urlParams.get('code');
 
-// Add the battle code to the page
-document.body.innerHTML += <p>Battle code: <strong>${battleCode}</strong></p>;
+console.log('battleCode:', battleCode); // Debug log
+
+if (battleCode) {
+  document.body.innerHTML += <p>Battle code: <strong>${battleCode}</strong></p>;
+} else {
+  document.body.innerHTML += <p style="color: red;">No battle code found in URL.</p>;
+}
