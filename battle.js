75c29cacb0d243b12aa05171f2ac5fb9094912cb
CoolCatt2000 <liamcouch74@icloud.com>
2025-05-26 21:27:11 +0100

    battle page

---
+++ battle.js
@@ -0,0 +1,9 @@
+// Get the battle code from the URL
+const urlParams = new URLSearchParams(window.location.search);
+const battleCode = urlParams.get('code');
+
+document.body.innerHTML += <p>Battle code: <strong>${battleCode}</strong></p>;// Get the battle code from the URL
+const urlParams = new URLSearchParams(window.location.search);
+const battleCode = urlParams.get('code');
+
+document.body.innerHTML += <p>Battle code: <strong>${battleCode}</strong></p>;
