fe6984d61f897f3f756ba627a02e926a043a8395
CoolCatt2000 <liamcouch74@icloud.com>
2025-05-27 22:23:20 +0100

    ,

--- battle.js
+++ battle.js
@@ -6,7 +6,7 @@ console.log('battleCode:', battleCode); // Debug log
 const displayElement = document.getElementById('battleCodeDisplay');
 
 if (battleCode) {
-  displayElement.innerHTML = `Battle code: <strong>${battleCode}</strong>`;
+  displayElement.innerHTML = Battle code: <strong>${battleCode}</strong>;
 } else {
   displayElement.innerHTML = <span style="color: red;">No battle code found in URL.</span>;
 }
