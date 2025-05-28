e8b43b0698855433ecf66570ecf93bcea1d6b859
CoolCatt2000 <liamcouch74@icloud.com>
2025-05-28 14:27:00 +0100

    .

--- battle.js
+++ battle.js
@@ -11,7 +11,6 @@ if (!battleCode) {
 
 displayElement.innerHTML = `Battle code: <strong>${battleCode}</strong>`;
 
-// Firebase presence reference
 const connectedRef = db.ref(".info/connected");
 const roomRef = db.ref('battles/' + battleCode);
 
@@ -21,25 +20,30 @@ connectedRef.on("value", (snap) => {
       const roomData = snapshot.val();
 
       if (!roomData || !roomData.player1) {
-        // Assign Player 1
-        roomRef.child("player1").set(true);
-        roomRef.child("player1").onDisconnect().remove();
-        displayElement.innerHTML += '<br>You are <strong>Player 1</strong>';
-        console.log("Player 1 has joined.");
+        // Safely assign Player 1
+        const playerRef = roomRef.child("player1");
+        playerRef.onDisconnect().remove().then(() => {
+          playerRef.set(true).then(() => {
+            displayElement.innerHTML += '<br>You are <strong>Player 1</strong>';
+            console.log("Player 1 has joined.");
+          });
+        });
       } else if (!roomData.player2) {
-        // Assign Player 2
-        roomRef.child("player2").set(true);
-        roomRef.child("player2").onDisconnect().remove();
-        displayElement.innerHTML += '<br>You are <strong>Player 2</strong>';
-        console.log("Player 2 has joined.");
+        // Safely assign Player 2
+        const playerRef = roomRef.child("player2");
+        playerRef.onDisconnect().remove().then(() => {
+          playerRef.set(true).then(() => {
+            displayElement.innerHTML += '<br>You are <strong>Player 2</strong>';
+            console.log("Player 2 has joined.");
+          });
+        });
       } else {
-        // Room full
         displayElement.innerHTML += '<br><span style="color:red;">This battle already has 2 players.</span>';
         console.log("Battle is full.");
       }
     }).catch((error) => {
       console.error("Error reading room data:", error);
       displayElement.innerHTML += '<br><span style="color:red;">Error loading battle data.</span>';
-    });
-  }
+    });
+  }
 });
