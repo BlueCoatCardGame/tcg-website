83a738b11753b0d1514b5860d22d9ce409a9ed20
CoolCatt2000 <liamcouch74@icloud.com>
2025-05-28 14:22:18 +0100

    .

--- battle.js
+++ battle.js
@@ -1,52 +1,45 @@
 const urlParams = new URLSearchParams(window.location.search);
 const battleCode = urlParams.get('code');
-
-console.log('battleCode:', battleCode); // Debug log
-
 const displayElement = document.getElementById('battleCodeDisplay');
 
-if (battleCode) {
-  displayElement.innerHTML = `Battle code: <strong>${battleCode}</strong>`;
+console.log('battleCode:', battleCode);
 
-  const roomRef = db.ref('battles/' + battleCode);
+if (!battleCode) {
+  displayElement.innerHTML = '<span style="color: red;">No battle code found in URL.</span>';
+  return;
+}
 
-  roomRef.once('value')
-    .then((snapshot) => {
+displayElement.innerHTML = `Battle code: <strong>${battleCode}</strong>`;
+
+// Firebase presence reference
+const connectedRef = db.ref(".info/connected");
+const roomRef = db.ref('battles/' + battleCode);
+
+connectedRef.on("value", (snap) => {
+  if (snap.val() === true) {
+    roomRef.once("value").then((snapshot) => {
       const roomData = snapshot.val();
 
-      if (!roomData) {
-        // This is Player 1
-        roomRef.set({
-          player1: true
-        }).then(() => {
-          displayElement.innerHTML += `<br>You are <strong>Player 1</strong>`;
-          console.log("Player 1 has joined.");
-
-          // Remove player1 if they disconnect
-          roomRef.child('player1').onDisconnect().remove();
-        });
+      if (!roomData || !roomData.player1) {
+        // Assign Player 1
+        roomRef.child("player1").set(true);
+        roomRef.child("player1").onDisconnect().remove();
+        displayElement.innerHTML += '<br>You are <strong>Player 1</strong>';
+        console.log("Player 1 has joined.");
       } else if (!roomData.player2) {
-        // This is Player 2
-        roomRef.update({
-          player2: true
-        }).then(() => {
-          displayElement.innerHTML += `<br>You are <strong>Player 2</strong>`;
-          console.log("Player 2 has joined.");
-
-          // Remove player2 if they disconnect
-          roomRef.child('player2').onDisconnect().remove();
-        });
+        // Assign Player 2
+        roomRef.child("player2").set(true);
+        roomRef.child("player2").onDisconnect().remove();
+        displayElement.innerHTML += '<br>You are <strong>Player 2</strong>';
+        console.log("Player 2 has joined.");
       } else {
-        // Room is full
-        displayElement.innerHTML += `<br><span style="color:red;">This battle already has 2 players.</span>`;
+        // Room full
+        displayElement.innerHTML += '<br><span style="color:red;">This battle already has 2 players.</span>';
         console.log("Battle is full.");
       }
-    })
-    .catch((error) => {
+    }).catch((error) => {
       console.error("Error reading room data:", error);
-      displayElement.innerHTML += `<br><span style="color:red;">Error loading battle data.</span>`;
+      displayElement.innerHTML += '<br><span style="color:red;">Error loading battle data.</span>';
     });
-
-} else {
-  displayElement.innerHTML = `<span style="color: red;">No battle code found in URL.</span>`;
-}
+  }
+});
